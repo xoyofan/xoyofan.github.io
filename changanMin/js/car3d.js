8b821f9a72448b1a25ea626334751514e0a0c37b
xoyofan <xooyoo@vip.qq.com>
2017-08-16 14:17:44 +0800

    add loading

--- changanMin/js/car3d.js
+++ changanMin/js/car3d.js
@@ -1,19 +1,39 @@
+'use strict'
 !function(){
 
-    if(!Detector.support3D){
-        alert('该浏览器不支持3D显示，请使用2D图片方案！')
-        return;
-    }
+    var c = 0;
+    var t;
 
-    var tmp = 1;
-    $("#menu").click(function() {
-         //$(".menu").toggle();
+    function timedCount()
+    {
+        
+        c=c+1;
+        if(c>100){
+            clearTimeout(t);
+        }else{
+            $('#LoadingProgress').css('width',c+'%');
+            t = setTimeout(timedCount,100);
+        }
+        
+
+    }
+    timedCount();
+
+
+	if(!Detector.support3D){
+		alert('该浏览器不支持3D显示，请使用2D图片方案！')
+		return;
+	}
+
+	var tmp = 1;
+	$("#menu").click(function() {
+		 //$(".menu").toggle();
          $(".bottomMenu").slideToggle("slow"); 
-         tmp *= -1;
-        $(this).css("transform", "rotateZ(" + tmp  * 180 + "deg)");
-        $(this).css("-webkit-transform", "rotateZ(" + tmp * 180 + "deg)");
-        $(this).css("-moz-transform", "rotateZ(" + tmp * 180 + "deg)");
-    });
+		 tmp *= -1;
+		$(this).css("transform", "rotateZ(" + tmp  * 180 + "deg)");
+		$(this).css("-webkit-transform", "rotateZ(" + tmp * 180 + "deg)");
+		$(this).css("-moz-transform", "rotateZ(" + tmp * 180 + "deg)");
+	});
     $(".menu").click(function() {
 
         
@@ -27,77 +47,75 @@
             $(this).children(".new-sub").slideDown("normal"); 
         }
     });
-    
+	
 
-    $('#carType .new-sub li').click(function() {
-
-        //车型
+    //车型
+	$('#carType .new-sub li').click(function() {
 
         var id = $(this).index();
-       
-        //精英型 轮毂1 无天窗
-        if (id == 0) {
+    
+        for(var key in ITEMS["carType"]){
 
-            viewer.setMultiVisible('dipei_lungu',true);
-            viewer.setMultiVisible('gaopei_lungu',false);
-            viewer.setMultiVisible('wuTianChuangcheding',true);
-            viewer.setMultiVisible('tianChuangcheding',false);
-           
-            viewer.setMeshVisible('yaoshikou',true);
-            viewer.setMeshVisible('yijianqidong',false);
-            viewer.setMeshVisible('wukongtiao',true);
-            //玻璃
-            viewer.HtMaterial['boli'].color = new THREE.Color(itemsBoliColor[0]);
+            var _value = ITEMS["carType"][key][id];
+
+            if(key == 'boli'){
+                viewer.HtMaterial['boli'].color = new THREE.Color(_value);
+            }else{    
+
+                var isMulti = ITEMS["carType"][key][3];        
+                if(isMulti == 1){
+                    viewer.setMultiVisible(key,_value);
+                }else{
+                    viewer.setMeshVisible(key,_value);
+                }
+            }
         }
-        //豪华型 轮毂1 天窗
-        else if (id == 1) {
+ 
 
-            viewer.setMultiVisible('dipei_lungu',true);
-            viewer.setMultiVisible('gaopei_lungu',false);
-            viewer.setMultiVisible('wuTianChuangcheding',false);
-            viewer.setMultiVisible('tianChuangcheding',true);
-      
-            viewer.setMeshVisible('yaoshikou',true);
-            viewer.setMeshVisible('yijianqidong',false);
-            viewer.setMeshVisible('wukongtiao',true);
-            //玻璃
-            viewer.HtMaterial['boli'].color = new THREE.Color(itemsBoliColor[0]);
-        }
-        //尊贵型 轮毂2 隐私玻璃
-        else if (id == 2) {
-
-            viewer.setMultiVisible('dipei_lungu',false);
-            viewer.setMultiVisible('gaopei_lungu',true);
-            viewer.setMultiVisible('wuTianChuangcheding',false);
-            viewer.setMultiVisible('tianChuangcheding',true);
-            
-            viewer.setMeshVisible('yaoshikou',false);
-            viewer.setMeshVisible('yijianqidong',true);
-            viewer.setMeshVisible('wukongtiao',false);
-            //玻璃
-            viewer.HtMaterial['boli'].color = new THREE.Color(itemsBoliColor[1]);
-        }
-
-    });
-
+	});
+    //开关车门
     $("#showParameter").click(function () {
         // var ui = document.getElementById('showParameter');
         // ui.style.visibility = "hidden";
         $("#showParameter").css('visibility',"hidden");
         viewer.doorAnimate(DOOR_INFO['LeftFront']);
     });
-        //出车门
 
+    var curColorId = 0;
+    //车身颜色
     var keys=['bai','yin','hong','hui'];
 
-    $('#bodyColor .new-sub li').click(function() {
+	$('#bodyColor .new-sub li').click(function() {
 
-        var id = $(this).index();
+		var id = $(this).index();
         curColorId = id;
 
-        viewer.updateMaterial('cheshen',itemsColors['cheshen'][keys[id]]);
-        viewer.updateMaterial('cheding', itemsColors['cheshen'][keys[id]]);
+        var params = ITEMS['mainColor']['cheshen'][keys[id]];
 
+		viewer.updateMaterial('cheshen',params);
+        viewer.updateMaterial('cheding',params);
+
+	});
+
+    //车顶颜色
+    $('#topColor .new-sub li').click(function() {
+
+        var id = $(this).index();
+     
+        //与车身同色
+        if (id == 0) {
+
+            viewer.updateMaterial('cheding',ITEMS['mainColor']['cheshen'][keys[curColorId]]);
+        }
+        //炭晶灰
+        else if (id == 1) {
+             viewer.updateMaterial('cheding',ITEMS['mainColor']['cheding']['hei']);
+        }
+        //纯白
+        else if (id == 2) {
+            viewer.updateMaterial('cheding',ITEMS['mainColor']['cheding']['bai']);
+        }
+        
     });
 
     //运动外观套件
@@ -107,62 +125,38 @@
        
         //运动套件无
 
-        if (id == 0) {
-            viewer.setMultiVisible('peijian_zhengti',false);
-            viewer.HtMaterial['kaqian'].color = new THREE.Color(0x222222);
-        }
-        else {
-            viewer.setMultiVisible('peijian_zhengti',true);
-            viewer.HtMaterial['peijian'].color = new THREE.Color(itemsPeijianColor[id]);
-            viewer.HtMaterial['kaqian'].color = new THREE.Color(0xaa001a);
-        }
+        var isVisible = id == 0 ? false : true;
+        viewer.setMultiVisible('peijian_zhengti',isVisible);
+
+        //卡钳颜色：无外观套件为灰，有则为红色
+        var kaqianColor = id == 0 ? ITEMS["kaqianColor"][0] : ITEMS["kaqianColor"][1];
+        viewer.HtMaterial['kaqian'].color = new THREE.Color(kaqianColor);
+        //套件颜色
+        viewer.HtMaterial['peijian'].color = new THREE.Color(ITEMS["peijianColor"][id]);
     });
     
-    $('#topColor .new-sub li').click(function() {
 
-        var id = $(this).index();
-     
-        //与车身同色
-        if (id == 0) {
-
-            viewer.updateMaterial('cheding',itemsColors['cheshen'][keys[curColorId]]);
-        }
-        //炭晶灰
-        else if (id == 1) {
-             viewer.updateMaterial('cheding',itemsColors['cheding']['hei']);
-        }
-        //纯白
-        else if (id == 2) {
-            viewer.updateMaterial('cheding',itemsColors['cheding']['bai']);
-        }
-        
-    });
 
     //玻璃
     $('#boli .new-sub li').click(function () {
 
         var id = $(this).index();
       
-        new TWEEN.Tween(viewer.HtMaterial['boli'].color).to(new THREE.Color(itemsBoliColor[id]), 1000).start();
+        new TWEEN.Tween(viewer.HtMaterial['boli'].color).to(new THREE.Color(ITEMS["boliColor"][id]), 1000).start();
        
     });
-    //玻璃
+	
 
     //天窗
     $('#tianchuang .new-sub li').click(function () {
-        var id = $(this).index();//$(this).attr("id");
-        //天窗无
-        if (id == 0) {
-            
-            viewer.setMultiVisible('wuTianChuangcheding',true);
-            viewer.setMultiVisible('tianChuangcheding',false);
-        }else {
-            
-            viewer.setMultiVisible('wuTianChuangcheding',false);
-            viewer.setMultiVisible('tianChuangcheding',true);
-        }
+
+        var id = $(this).index();
+
+        var hasTianchuang = id == 0 ? false : true;
+        viewer.setMultiVisible('wuTianChuangcheding',!hasTianchuang);
+        viewer.setMultiVisible('tianChuangcheding',hasTianchuang);
+
     });
-        //天窗
 
 
     //拉花
@@ -189,7 +183,7 @@
 
                 viewer.HtMaterial['lahua'].map = viewer.HtData.texture[name].node;
             }
-        })(itemsLaHua[id]);
+        })(ITEMS["lahua"][id]);
        
 
     });
@@ -207,15 +201,14 @@
 }();
 
 
-
 !function(){
 
-    var filePath = Detector.isMobile() ? 'mobile/' : 'pc/';//判断当前设备是否为手机，非平板和PC。因为将要采用两套模型和贴图，按此区分。
-    viewer = new carViewer('goodsViewer',filePath);
+	var filePath = Detector.isMobile() ? 'mobile/' : 'pc/';//判断当前设备是否为手机，非平板和PC。因为将要采用两套模型和贴图，按此区分。
+	viewer = new carViewer('goodsViewer',filePath);
 
-    viewer.init();
+	viewer.init();
 
-    
+	
     var envCubeMap2 = viewer.initCubeMap('images/lightBr/','.jpg',0);
 
     var envCubeMap = viewer.initCubeMap('images/cube_hw/','.jpg',0);
@@ -227,6 +220,6 @@
     viewer.initMaterials(envCubeMap,envCubeMap2);
     viewer.loadHtFiles('ht/');
 
-    //viewer.initGui('cheshen',itemsColors['cheshen']['hong']);
+   // viewer.initGui('cheshen',ITEMS['mainColor']['cheshen']['hong']);
 
 }();
